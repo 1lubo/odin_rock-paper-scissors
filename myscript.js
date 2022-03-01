c8f9c483308baad2080202a9ccb16dd3b16b4fae
1Lubo <1Lubo@pm.me>
2022-03-01 17:03:37 +0100

    add playRound function

--- myscript.js
+++ myscript.js
@@ -6,3 +6,21 @@ let computerPlay = function(){
     return computerChoice;
 }
 
+let playRound = function (computerC, playerC){
+    
+    let playerSelection = playerC.toLowerCase();
+    let computerSelection = computerC.toLowerCase();
+
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "You win! Rock beats scissors.";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats rock.";
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissors beats paper.";
+    } else if (playerSelection === computerSelection) {
+        return "Draw.";
+    } else {
+        return `You lose. ${computerSelection} beats ${playerSelection}.`
+    }
+}
+
