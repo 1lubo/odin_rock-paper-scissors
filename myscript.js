51529bc3e55a98230ca4e96c105d2c2fef6e98f5
1Lubo <1Lubo@pm.me>
2022-03-01 17:18:34 +0100

    add game function

--- myscript.js
+++ myscript.js
@@ -12,15 +12,36 @@ let playRound = function (computerC, playerC){
     let computerSelection = computerC.toLowerCase();
 
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You win! Rock beats scissors.";
+        console.log("You win! Rock beats scissors.");
+        return 'P';
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "You win! Paper beats rock.";
+        console.log("You win! Paper beats rock.");
+        return 'P';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "You win! Scissors beats paper.";
+        console.log("You win! Scissors beats paper.");
+        return 'P';
     } else if (playerSelection === computerSelection) {
-        return "Draw.";
+        console.log("Draw.");
+        return 'D';
     } else {
-        return `You lose. ${computerSelection} beats ${playerSelection}.`
+        console.log(`You lose. ${computerSelection} beats ${playerSelection}.`);
+        return "C";
     }
 }
 
+let game = function () {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerS = prompt("Rock-Paper-Scissors. What do you choose?");
+        let result = (playRound(computerPlay(), playerS));
+        if (result === 'P') {
+            playerScore++ ;
+        } else if (result === 'C') {
+            computerScore++
+        }
+    }
+    console.log(`The final score is: Player ${playerScore} / Computer ${computerScore}`)
+}
+
+game()
